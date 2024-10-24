375dd85930be1c2eb4dca36a6968ef4fa2f70fbd
fui96 <kidwelqn@mail.uc.edu>
2024-10-23 21:05:12 -0400

    innit

--- src/DevTest.java
+++ src/DevTest.java
@@ -16,10 +16,29 @@ public class DevTest {
         Age = SafeInput.getInt(in, "Please enter your age");
         System.out.println("You are "+Age +" years old");
     }*/
-    public static void main(String[] args) {
+    /*public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         Double Decimal = 0.0;
         Decimal = SafeInput.getDouble(in, "Please enter a decimal");
         System.out.println("The decimal you chose is " + Decimal);
+    }*/
+    /*public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int within = 0;
+        within = SafeInput.getRangedInt(in, "Please enter a number between 1 and 10", 1,10);
+        System.out.println("Your number between 1 and 10 is " + within);
+
+   }*/
+    /*public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        double within = 0.0;
+        within = SafeInput.getRangedDouble(in,"Please enter a decimal between 0 and 1", 0,1);
+        System.out.println("The decimal you chose is " + within);
+    }*/
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        Boolean Cont = null;
+        Cont = SafeInput.getYNConfirm(in, "Would you like to continue? [y/n]");
+        System.out.println("You have chosen continue "+ Cont);
     }
 }
--- src/SafeInput.java
+++ src/SafeInput.java
@@ -49,4 +49,81 @@ public class SafeInput {
         }while(!Valid);
         return retDouble;
     }
+    public static int getRangedInt(Scanner pipe, String prompt, int low , int high){
+        int retInt = 0;
+        Boolean Valid = false;
+        do{
+            System.out.print("\n" +prompt + ": ");
+            if(pipe.hasNextInt()){
+                retInt = pipe.nextInt();
+                pipe.nextLine();
+                if (retInt >= low && retInt <= high){
+                    Valid = true;
+                }
+                else{
+                    System.out.println("Invalid input, please enter a value within the range");
+                }
+            }
+            else{
+                System.out.println("Invalid input, please enter an integer");
+                pipe.next();
+            }
+        }while(!Valid);
+
+        return retInt;
+    }
+    public static Double getRangedDouble(Scanner pipe, String prompt, double low , double high){
+        Double retDouble = 0.0;
+        Boolean Valid = false;
+
+        do {
+            System.out.print("\n" +prompt + ": ");
+            if(pipe.hasNextDouble()){
+                Double FloatRet;
+                FloatRet = pipe.nextDouble();
+                pipe.nextLine();
+                if (FloatRet >= low && FloatRet <= high){
+                    Valid = true;
+                    retDouble = FloatRet;
+                }
+                else{
+                    System.out.println("Invalid input, please enter a value within the range");
+                }
+            }
+            else{
+                System.out.println("Invalid input, please enter a double");
+                pipe.next();
+            }
+        }while(!Valid);
+
+        return retDouble;
+    }
+    public static Boolean getYNConfirm(Scanner pipe, String prompt){
+        Boolean retBool = null;
+        String FloatRet;
+        Boolean Valid = false;
+        do{
+            System.out.print("\n" +prompt + ": ");
+            if(pipe.hasNext()){
+                FloatRet = pipe.next();
+                pipe.nextLine();
+                if(FloatRet.toUpperCase().equals("Y")){
+                    retBool = true;
+                    Valid = true;
+                }
+                else if (FloatRet.toUpperCase().equals("N")){
+                    retBool = false;
+                    Valid = true;
+                }
+                else{
+                    System.out.println("Invalid Input, please enter a valid value");
+                    Valid = false;
+                }
+            }
+            else{
+                System.out.println("Invalid input, try again");
+            }
+        }while(!Valid);
+        return retBool;
+    }
 }
